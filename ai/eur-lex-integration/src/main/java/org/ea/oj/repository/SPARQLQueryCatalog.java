efb9dba8f1f97a22df9a4bb96aaf40ee137e451f
Ermal ALIRAJ <ermal.aliraj@gmail.com>
2022-01-23 23:34:28 +0100

    eur-lex Import Acts from OJ - completed

--- ai/eur-lex-integration/src/main/java/org/ea/oj/repository/SPARQLQueryCatalog.java
+++ ai/eur-lex-integration/src/main/java/org/ea/oj/repository/SPARQLQueryCatalog.java
@@ -1,4 +1,4 @@
-package org.ea.oj.service;
+package org.ea.oj.repository;
 
 import org.apache.jena.query.ParameterizedSparqlString;
 import org.apache.jena.query.Query;
@@ -65,7 +65,37 @@ public class SPARQLQueryCatalog {
                         "\t\t\t}\n" +
                         "\t}\n" +
                         "\tBIND(IF(BOUND(?manif_fmx4),IRI(concat(?manif_fmx4,\"/DOC_2\")),\"\") as ?fmx4_act)\n" +
-                        "} limit 300");
+                        "}");
+        return queryStr.asQuery();
+    }
+
+    public static Query getAllActsForYear(String type, int year) {
+        ParameterizedSparqlString queryStr = new ParameterizedSparqlString();
+        queryStr.setNsPrefix("cdm", "http://publications.europa.eu/ontology/cdm#");
+        queryStr.setNsPrefix("owl", "http://www.w3.org/2002/07/owl#");
+        queryStr.append(
+                "SELECT DISTINCT substr(str(?OJ),52,3) substr(str(?OJ),58,4) ?OJ ?date_document ?fmx4_act \n" +
+                        "WHERE\n" +
+                        "  { ?work cdm:resource_legal_eli ?eli ; cdm:work_date_document ?date_document\n" +
+                        "\t\tFILTER(regex(str(?eli),'/" + type + "/'))\n" +
+                        "\t\tFILTER(substr(str(?date_document), 1, 4) IN (\"" + year + "\"))\n" +
+                        "    ?work owl:sameAs  ?OJ\n" +
+                        "    FILTER regex(str(?OJ), \"/oj/\")\n" +
+                        "    OPTIONAL\n" +
+                        "      { ?exp  cdm:expression_title ?title ;\n" +
+                        "              cdm:expression_uses_language ?lang ;\n" +
+                        "              cdm:expression_belongs_to_work ?work\n" +
+                        "        FILTER(?lang = <http://publications.europa.eu/resource/authority/language/ENG>)\n" +
+                        "        OPTIONAL\n" +
+                        "          { ?manif_fmx4 cdm:manifestation_manifests_expression  ?exp ;\n" +
+                        "\t\t\t\t\t\tcdm:manifestation_type  ?type_fmx4\n" +
+                        "            FILTER(str(?type_fmx4) = \"fmx4\")\n" +
+                        "\t\t\tBIND(STRLEN(?exp) AS ?len_o)  \n" +
+                        "          }\n" +
+                        "      }\n" +
+                        "\t  FILTER(?len_o != 0) \n" +
+                        "    BIND(if(bound(?manif_fmx4), iri(concat(?manif_fmx4, \"/DOC_2\")), \"\") AS ?fmx4_act)\n" +
+                        "  }\n");
         return queryStr.asQuery();
     }
 }
